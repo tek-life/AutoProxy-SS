875d337f72411fb3f126a5a4db350c02696c1d3c
Haifeng Li <omycle@gmail.com>
2014-06-28 15:56:28 +0800

    The first Demo.
    Should Also check available

--- ss.py
+++ ss.py
@@ -1,21 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import socks
+import socket
+import urllib2
+import datetime
+import pdb
+import subprocess
+
+def _Config_proxy_():
+  socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
+  socket.socket = socks.socksocket
+#print urllib2.urlopen('http://www.sexinsex.net').read()
+_Config_proxy_()
+
+def _Check_Speed_():
+  speed=[]
+  for i in range(3):
+    start = datetime.datetime.now()
+    content = urllib2.urlopen('https://www.google.com').read()
+#    print type(content)
+#    print len(content)
+    end = datetime.datetime.now()
+    delta = (end-start).seconds*1000.0+(end-start).microseconds
+    _speed = len(content)*1000.0/delta
+    if _speed < 100:
+      print("%d -- %d" %(delta,_speed))
+      speed.append(_speed)
+  return sum(speed)/len(speed)
+
+#print _Check_Speed_()
 
 config = open("id.ini","r")
 content = config.readlines()
 content = [co[:-1:] for co in content]
-
+#print content
+#pdb.set_trace()
 def _Read_One_Item(list_part, num):
   if num < 4:
     print("Error! The item is wrong\n")
     exit(-1);
-  ip=list_part[0]
-  port=list_part[1]
+  ip=list_part[0]#.encode("utf-8")
+  ip=re.findall(r'：(\d*.{3,}\d+)',ip)
+  port=list_part[1]#.encode("utf-8")
+  port=re.findall(r'：(\d+)',port)
   password=list_part[2]
+  password=re.findall(r'：(.+)',password)
   encry = list_part[3]
+  encry = re.findall(r'：(.+)',encry)
   supporter = list_part[4]
+  supporter = re.findall(r'：(.+)',supporter)
 #  print [ip,port,password,encry,supporter]
-  return [ip,port,password,encry,supporter]
+  return_value = {"ip":"".join(ip),"port":"".join(port),"password":"".join(password),"encry":"".join(encry),"supporter":"".join(supporter)}
+  return return_value
 
-
+def Connect():
+  pass
 
 def _Check_Content():
   for fo in content:
@@ -27,11 +68,42 @@ def foo():
   iter_i = 0
   temp = []
   temp = content
+  max_speed=0
+  temp_speed=0.0
+  max_config=[]
+  nex=0
+ 
   while iter_i < length:
-    print _Read_One_Item(temp,5)
+    str_li=[]
+    _config_li= _Read_One_Item(temp,5)
+    str_li.append("sslocal")
+#    str_li.append(_config_li["ip"])
+    str_li.append("-s")
+    str_li.append(_config_li["ip"])
+    str_li.append("-p")
+    str_li.append(_config_li["port"])
+    str_li.append("-k")
+    str_li.append(_config_li["password"])
+    str_li.append("-m")
+    str_li.append(_config_li["encry"])
+    str_li.append("-l")
+    str_li.append("1083")
+#    print str_li
+    child=subprocess.Popen(str_li)
+    temp_speed = _Check_Speed_()
+    print("IP:%s 's speed %d \n"%(_config_li["ip"],temp_speed))
+    child.kill()
+    if temp_speed > max_speed:
+       max_config=str_li[:]
+       max_speed=temp_speed
+#       max_str_config=str_li
 #    print iter_i
     iter_i+=5
     temp=temp[5: ]
+    nex += 1
+  
+  print("WIN: %s -- %d\n"%("".join(max_config),max_speed))
+  child=subprocess.Popen(max_config)
 
 if __name__ == '__main__':
   foo()
